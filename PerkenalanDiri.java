ba6ab91a575e6cfcae18916f9d0ca7373367cb80
Ismaturrofiah <73883487+Ismaturrofiah@users.noreply.github.com>
2020-11-12 07:07:15 +0700

    Add files via upload

---
+++ PerkenalanDiri.java
@@ -0,0 +1,20 @@
+public class PerkenalanDiri {
+	public static void main(String[] args) {
+		String NIM; //deklarasi variabel
+		NIM = "V3420042"; //inisialisasi var(beri nilai)
+		String Nama; //deklarasi variabel
+		Nama = "Ismaturrofiah"; //inisialisasi var(beri nilai)
+		int Umur; //deklarasi langsung
+		Umur = 19;  //inisialisasi var(beri nilai)
+		double Tinggi = 1.55; //deklarasi sekaligus inisialisasi
+		char JenisKelamin = 'P';
+		boolean mahasiswaaktif = true;
+		System.out.println("Data Mahasiswa");
+		System.out.println("NIM : "+NIM ); //cetak ver
+		System.out.println("Nama Lengkap : "+Nama);
+		System.out.println("Umur : "+Umur+" tahun");
+		System.out.println("Tinggi Badan : "+Tinggi+" Meter" );
+		System.out.println("Jenis Kelamin : "+JenisKelamin );
+		System.out.println("Mahasiswa Aktif : "+mahasiswaaktif);
+	}
+}
